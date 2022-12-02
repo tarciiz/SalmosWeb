fd1a879b02353ec70a574f5a8b1ed506df86ed60
rockijulius <julius.teixeira@gmail.com>
2022-12-01 23:23:26 -0300

    pra fazer pedido

--- src/main/java/br/com/ifba/pedido/model/Pedido.java
+++ src/main/java/br/com/ifba/pedido/model/Pedido.java
@@ -9,6 +9,7 @@ import br.com.ifba.infrastructure.model.PersistenceEntity;
 import br.com.ifba.item.model.Item;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
@@ -16,7 +17,6 @@ import javax.persistence.Table;
 
 import java.util.List;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 
 /**
  *
@@ -26,13 +26,12 @@ import lombok.EqualsAndHashCode;
 @Entity
 @Table(name = "pedido")
 @Data
-@EqualsAndHashCode(callSuper = false)
 public class Pedido extends PersistenceEntity {    
     private String notaFiscal;
     private double valorTotal;
     
-    @ManyToOne
-    @JoinColumn(name = "empenho_id")
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "empenho_id", referencedColumnName = "ID")
     private Empenho empenho;
     
     @ManyToMany
